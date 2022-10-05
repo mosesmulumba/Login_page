914455a028cd64b6c2cb3b166cbf012be58d3f1e
moses <mulumbamoses94@gmail.com>
2022-10-05 10:24:00 +0300

    login page

---
+++ src/nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Nav extends React.Component{
+    render(){
+        return(
+            <div className="nav--top">
+                <img src="crane-cloud block.jpeg" alt="logo" />
+            </div>
+        );
+    }
+    
+}
+
+export default Nav;
